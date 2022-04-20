a51b645b3322da26985247af51d948cf731eb465
Norman B. Lancaster <qbradq@gmail.com>
2022-04-20 02:31:49 -0500

    Added enemy hurt animation

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -7,10 +7,18 @@ export var walk_speed:float = 50.0
 
 var velocity:Vector2
 var facing_left:bool
+var iframes:float
 
 
 func _physics_process(delta):
+	iframes -= delta
+	iframes = max(0.0, iframes)
+	if iframes:
+		$Sprite.material.set_shader_param("white_progress", 1.0)
+	else:
+		$Sprite.material.set_shader_param("white_progress", 0.0)
 	think(delta)
+	$Sprite.flip_h = facing_left
 	velocity += Global.gravity_force * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 
@@ -24,3 +32,11 @@ func walk():
 		velocity.x = -walk_speed
 	else:
 		velocity.x = walk_speed
+	$AnimationPlayer.play("Walk")
+
+
+func hurt(damage:int):
+	if iframes:
+		return
+	iframes += 0.4
+	$AnimationPlayer.play("Hurt")
--- Player/PlayerBullet.gd
+++ Player/PlayerBullet.gd
@@ -6,3 +6,8 @@ export var velocity:Vector2 = Vector2.RIGHT * 400.0
 
 func _physics_process(delta):
 	global_position += velocity * delta
+
+
+func _on_PlayerBullet_body_entered(body):
+	if body is Enemy:
+		body.hurt(1)
